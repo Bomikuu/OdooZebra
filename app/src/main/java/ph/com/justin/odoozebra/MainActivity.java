50d0db4d2d6f95b175c16dc78ebbe1bc5aac0c09
Mico A. Dahang <mico@apollo.com.ph>
2018-06-20 11:45:32 +0800

    Updated UI

--- app/src/main/java/ph/com/justin/odoozebra/MainActivity.java
+++ app/src/main/java/ph/com/justin/odoozebra/MainActivity.java
@@ -6,6 +6,10 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -18,6 +22,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle(null);
+
+        Toolbar topToolBar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(topToolBar);
+        topToolBar.setLogo(R.drawable.ic_android_black_24dp);
+        topToolBar.setLogoDescription(getResources().getString(R.string.logo_desc));
     }
 
     class SyncStudents extends AsyncTask<String, String, String> {
@@ -80,5 +91,34 @@ public class MainActivity extends AppCompatActivity {
 
             progressDialog.dismiss();
         }
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if(id == R.id.action_refresh){
+            Toast.makeText(MainActivity.this, "Refresh App", Toast.LENGTH_LONG).show();
+        }
+        if(id == R.id.action_new){
+            Toast.makeText(MainActivity.this, "Create Text", Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
