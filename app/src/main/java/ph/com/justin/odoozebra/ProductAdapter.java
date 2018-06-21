9b84433dbd5c125f574e542e2cf9522489b9f73d
Justin R. Carretas <jst.carretas@gmail.com>
2018-06-21 15:26:31 +0800

    Created two views *created view for product details *created view for product list

--- app/src/main/java/ph/com/justin/odoozebra/ProductAdapter.java
+++ app/src/main/java/ph/com/justin/odoozebra/ProductAdapter.java
@@ -10,10 +10,10 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PickingAdapter extends RecyclerView.Adapter<PickingAdapter.MyViewHolder> {
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.MyViewHolder> {
 
     private Context mContext;
-    private List<PickingModel> pickingModels;
+    private List<ProductModel> productModels;
     private OnItemClickListener mListener;
 
     public interface OnItemClickListener {
@@ -27,13 +27,15 @@ public class PickingAdapter extends RecyclerView.Adapter<PickingAdapter.MyViewHo
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
-        TextView txtShipment, txtCompany;
+        TextView txtProductName, txtQuantityOrdered, txtQuantityToDo, txtQuantityDone;
 
         public MyViewHolder(View view, final OnItemClickListener listener) {
             super(view);
 
-            txtShipment = view.findViewById(R.id.txtShipment);
-            txtCompany = view.findViewById(R.id.txtCompany);
+            txtProductName = view.findViewById(R.id.txtProductName);
+            txtQuantityOrdered = view.findViewById(R.id.txtQuantityOrdered);
+            txtQuantityToDo = view.findViewById(R.id.txtQuantityToDo);
+            txtQuantityDone = view.findViewById(R.id.txtQuantityDone);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -65,30 +67,32 @@ public class PickingAdapter extends RecyclerView.Adapter<PickingAdapter.MyViewHo
         }
     }
 
-    public PickingAdapter(Context mContext, List<PickingModel> pickingModels) {
+    public ProductAdapter(Context mContext, List<ProductModel> productModels) {
         this.mContext = mContext;
-        this.pickingModels = pickingModels;
+        this.productModels = productModels;
     }
 
 
     @Override
-    public PickingAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ProductAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.picking_list, parent, false);
+                .inflate(R.layout.product_details, parent, false);
 
         return new MyViewHolder(itemView, mListener);
     }
 
 
     @Override
-    public void onBindViewHolder(final PickingAdapter.MyViewHolder holder, int position) {
-        holder.txtShipment.setText("Transfer Name: "+ pickingModels.get(position).getName());
-        holder.txtCompany.setText("Merchant: " + pickingModels.get(position).getPartner());
+    public void onBindViewHolder(final ProductAdapter.MyViewHolder holder, int position) {
+        holder.txtProductName.setText("Product Name:" + productModels.get(position).getName());
+        holder.txtQuantityOrdered.setText("Qty Ordered:" + productModels.get(position).getQty_ordered());
+        holder.txtQuantityToDo.setText("Qty to do:" + productModels.get(position).getProduct_qty());
+        holder.txtQuantityDone.setText("Qty done:" + productModels.get(position).getQty_done());
     }
 
     @Override
     public int getItemCount() {
-        return pickingModels.size();
+        return productModels.size();
     }
 }
 
